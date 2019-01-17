22781dff442a05ccfbd51992b569e1835a449d13
sunny <sunny@gmail.com>
2019-01-17 09:52:51 +0800

    delete file

--- com-gameportal-pay/src/main/java/com/gameportal/pay/utils/FCSOpenApiClient.java
+++
@@ -1,9 +0,0 @@
-package com.gameportal.pay.utils;
-
-import com.gameportal.pay.model.fcs.request.FCSOpenApiRequest;
-import com.gameportal.pay.model.fcs.response.FCSOpenApiResponse;
-
-public abstract interface FCSOpenApiClient {
-	public abstract FCSOpenApiResponse excute(FCSOpenApiRequest paramFCSOpenApiRequest, String paramString)
-			throws Exception;
-}
